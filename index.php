e6455e6995e878a09e32ed54b8927874437e77bf
pantazis <pantazisvastardis@yahoo.com>
2021-07-19 11:31:22 +0300

    test

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./public/css/style.css">
     
 
     <title>Document</title>
 </head>
 <body>
+    <?php include "./_include/nav.php" ?>
     <div class="conteiner">
         <div class="row">
             <div class="col-md-4">1</div>
@@ -19,10 +20,10 @@
         </div>
     </div>
     
-    <script src="js/libraries/popper.min.js"></script>
-    <script src="js/libraries/bootstrap.min.js"></script>
-    <script src="js/libraries/is.min.js"></script>   
-    <script src="js/libraries/all.min.js"></script>
+    <script src="./public/js/libraries/popper.min.js"></script>
+    <script src="./public/js/libraries/bootstrap.min.js"></script>
+    <script src="./public/js/libraries/is.min.js"></script>   
+    <script src="./public/js/libraries/all.min.js"></script>
    
     
 </body>

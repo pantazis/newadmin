fbb92624e473ae1276de3ccf359e9c5908d3c095
pantazis <pantazisvastardis@yahoo.com>
2021-07-19 16:10:37 +0300

    test

--- index.php
+++ index.php
@@ -32,13 +32,46 @@
   "blue_purple",
   "dark_grey",
   "dark_blue",
-  "black_blue"]
+  "black_blue"];
+
+  $arr2=[
+    "roboto-medium-13px",
+    "roboto-medium-13px",
+    "roboto-regular-15px",
+    "roboto-bold-30px",
+    "roboto-bold-27px",
+    "roboto-medium-37px",
+    "breadcrump",
+    "active-tab",
+    "tab-label-active",
+    "roboto-bold-14px",
+    "table-row-labels",
+    "table-row-data",
+    "roboto-medium-9px",
+    "roboto-medium-14px",
+    "roboto-medium-14px",
+    "roboto-bold-16px",
+    "roboto-bold-16px",
+    "roboto-bold-16px",
+    "roboto-bold-16px",
+    "roboto-regular-13px",
+    "roboto-bold-13px",
+    "roboto-regular-13px",
+    "label-text-form",
+    "roboto-bold-15px",
+    "roboto-regular-12px",
+
+  
+  ];
   ?>
   
    
 
 <?php foreach($arr as $value ){ ?>
 <button type="button" class="btn btn-<?=$value?>"><?=$value?></button>
+<?php }?>
+<?php foreach($arr2 as $value ){ ?>
+<p class="<?=$value?>"><?=$value?></p>
 <?php }?>
 <h1>asasas</h1>
 <p>asas</p>

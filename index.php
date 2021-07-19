3f3511bbf129eb4313f8ea91dd57d1bfe5bc1bd6
pantazis <pantazisvastardis@yahoo.com>
2021-07-19 15:16:42 +0300

    test

--- index.php
+++ index.php
@@ -11,25 +11,39 @@
 </head>
 <body>
     <?php include "./_include/nav.php" ?>
-    <div class="conteiner">
-        <div class="row">
-            <div class="col-md-4">1</div>
-            <div class="col-md-4">2</div>
-            <div class="col-md-4">3</div>
-            <div class="col-md-4">4</div>
-        </div>
+
+<?php $arr =["primary",
+  "secondary",
+  "success",
+  "info",
+  "warning",
+  "danger",
+  "light",
+  "dark",
+  "ice",
+  "light_grey",
+  "pal_pink",
+  "green",
+  "sky_blue",
+  "orange",
+  "pomegranade",
+  "blue_grey",
+  "red",
+  "blue_purple",
+  "dark_grey",
+  "dark_blue",
+  "black_blue"]
+  ?>
+  
+   
+
+<?php foreach($arr as $value ){ ?>
+<button type="button" class="btn btn-<?=$value?>"><?=$value?></button>
+<?php }?>
+<h1>asasas</h1>
+<p>asas</p>
 
 
-        <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
     </div>    
     <script src="./public/js/libraries/popper.min.js"></script>
     <script src="./public/js/libraries/bootstrap.min.js"></script>

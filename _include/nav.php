f6c0d4aeaad14904389b2463959fb20ef464c464
pantazis <pantazisvastardis@yahoo.com>
2021-07-20 14:21:17 +0300

    test

--- _include/nav.php
+++ _include/nav.php
@@ -1,6 +1,11 @@
 
 <nav class="sidebar top">
-    <div class="logo"></div>
+    <div class="logo">
+        <div class="burger-menu-container">
+            <img class="curve c1" src="./public/img/nav/1menu_bg.svg" alt="">
+            <img class="curve c2" src="./public/img/nav/2menu_bg.svg" alt="">
+        </div>
+    </div>
     <div class="nav-container">
 
     </div>    

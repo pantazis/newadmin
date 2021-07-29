fb96fa83b2ab42397d2c008ceda5afcd17558b4e
pantazis <pantazisvastardis@yahoo.com>
2021-07-29 17:00:26 +0300

    test

--- _include/nav.php
+++ _include/nav.php
@@ -35,7 +35,12 @@
 
         </div>
         <div class="nav-list search">
-            1
+            <div class="search-cont">
+                <span class="search-text">Domains <div class="icon-fi-sr-caret-down navdown"></div></span>
+            <input class="search-top" type="search" name="" id="">
+            </div>
+          
+            
 
         </div>
         <div class="nav-list user-icons">

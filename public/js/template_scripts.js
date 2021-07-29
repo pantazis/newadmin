6f05a9fcf059c9cab8798585978abb333ff8a63d
pantazis <pantazisvastardis@yahoo.com>
2021-07-29 09:19:13 +0300

    test

--- public/js/template_scripts.js
+++ public/js/template_scripts.js
@@ -24,18 +24,26 @@
     });
 
     var arr= [];
+    var i = 0;
     $(".nav-list-item2").click(function(){
         arr.push(this);
-        debugger;
-        
        
         $(".nav-list-item2").removeClass("opennav1");
+        
         if( arr.length >= 2 && arr[arr.length-1]==arr[arr.length-2]){
-            $(this).removeClass("opennav1");
+            
+            i++
         }else{
+            i=0;
+        }
+        if(i%2==0){
             $(this).addClass("opennav1");
+            }else{
+            $(this).removeClass("opennav1");
 
-        }    
+            }
+       
+            
                 
        
         

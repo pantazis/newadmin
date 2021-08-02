ad0efe4393e5cfa8fd49f07c1ac05ce1c28c2f41
pantazis <pantazisvastardis@yahoo.com>
2021-08-02 11:53:51 +0300

    test

--- public/js/template_scripts.js
+++ public/js/template_scripts.js
@@ -25,55 +25,51 @@
 
     //top nav popup open close
 
-    function menu_subcategory_open (parent_Element,open_class){
+    function menu_subcategory_open (parent_Element,open_class,listChildElement){
   
     var top_nav= {};
     top_nav.arr=[];    
     top_nav.num = 0;
-    top_nav.isopen = false;
+ 
     $(parent_Element).click(function(){ 
         
          
         if(top_nav.arr.length>1){
             top_nav.arr.shift();
         }
-        top_nav.arr.push(this);
-        
-       
-        $(parent_Element).removeClass(open_class);
-      
+        top_nav.arr.push(this);       
+        $(parent_Element).removeClass(open_class);      
         
         if( top_nav.arr.length >= 2 && top_nav.arr[top_nav.arr.length-1]==top_nav.arr[top_nav.arr.length-2]){
-            
             top_nav.num++
         }else{
             top_nav.num=0;
         }
-        if(top_nav.isopen==true){
-            top_nav.num = 1;
-
-        }
+       
         
         if(top_nav.num%2==0){
             $(this).addClass(open_class);
             }else{
             $(this).removeClass(open_class);
-            }
+        }
 
-            top_nav.isopen =false;
+          
           
 
     });
 
     $("body").click(function(e){
-        if($(e.target).closest(parent_Element).length==0){          
-            
-            var element = top_nav.arr[top_nav.arr.length-1];
-            top_nav.isopen =true;           
-            $(element).click();
-           
+        var element = top_nav.arr[top_nav.arr.length-1];
+       
+        if($(e.target).closest(parent_Element).length==0 ){             
+            $(element).removeClass(open_class); 
 
         };
+        if($(e.target).closest(listChildElement).length!=0 ){            
+            $(element).addClass(open_class);
+        };
+      
+      
     })
 }
     
@@ -85,7 +81,7 @@
 
 
 
-menu_subcategory_open (".nav-list-item","opennav1");
+menu_subcategory_open (".nav-list-item","opennav1",".custom-popup");
 //menu_subcategory_open (".nav-list-item","opennav1")
 
 

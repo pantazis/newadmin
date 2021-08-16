61b13044a3d74272688244716bfa1a5cdba29fa6
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-16 12:22:58 +0300

    Update

--- _include/modals.php
+++ _include/modals.php
@@ -10,10 +10,10 @@ $input = ["#0001 An Ubavicius", "#0005 Γιάννης Παπαδόπουλος",
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Πρόκειται να απενεργοποιήσετε το λογαριασμό του χρήστη <?php echo $input[rand(0,4)] ?></p>
+        <p>Πρόκειται να απενεργοποιήσετε το λογαριασμό του χρήστη <br> <?php echo $input[rand(0,4)] ?></p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Κλείσιμο</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Κλείσιμο</button> 
         <button type="button" id="setactive" class="btn btn-primary"><span class="activeText">Απενεργοποίηση</span><span class="notActiveText">Eνεργοποίηση</span></button>
       </div>
     </div>

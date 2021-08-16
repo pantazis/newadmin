95665eb2273eb6477e966d8c1a914d6d5351f9b3
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-16 11:36:38 +0300

    modal update

--- _include/modals.php
+++ _include/modals.php
@@ -1,3 +1,7 @@
+<?php
+$input = ["#0001 An Ubavicius", "#0005 Γιάννης Παπαδόπουλος", "#0471 Κώστας Δάσκαλος", "#0165 Παντελής Ζαννίκος", "#0006 Γιώργος Θειακός"];
+?>
+
 <div class="modal fade" id="disableAccount" tabindex="-1" aria-labelledby="disableAccountLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -6,11 +10,11 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Woohoo, you're reading this text in a modal!</p>
+        <p>Πρόκειται να απενεργοποιήσετε το λογαριασμό του χρήστη <?php echo $input[rand(0,4)] ?></p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Κλείσιμο</button>
+        <button type="button" class="btn btn-primary">Απενεργοποίηση</button>
       </div>
     </div>
   </div>

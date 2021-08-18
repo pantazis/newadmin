42b49f20e9ab7ea64ab0864c78f8096500e926ce
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-18 11:43:46 +0300

    php pages - routing

--- _include/root.php
+++ _include/root.php
@@ -4,11 +4,35 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case $request :      
-        include '_views/home.php';
+        include '_views/dashboard.php';
         break;   
-    case $request.'about' :
-        include '_views/about.php';
+    case $request.'domains' :
+        include '_views/table-list.php';
         break;
+    case $request.'domain' :
+        include '_views/domain-view.php';
+        break; 
+    case $request.'orders' :
+        include '_views/table-list.php';
+        break;  
+    case $request.'order' :
+        include '_views/order-view.php';
+        break;
+    case $request.'ssl' :
+        include '_views/ssl-view.php';
+        break; 
+    case $request.'ssl-list' :
+        include '_views/table-list.php';
+        break;
+    case $request.'billing-profile' :
+        include '_views/billing-profile.php';
+        break; 
+    case $request.'billing-profiles' :
+        include '_views/table-list.php';
+        break;
+    case $request.'order-doc' :
+        include '_views/order-doct.php';
+        break;             
   //  default:
       //  http_response_code(404);
        // include '/views/404.php';

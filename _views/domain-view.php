d29dd7a5b8a472fa30a914f915a1a224c88f8830
pantazis <pantazisvastardis@yahoo.com>
2021-08-19 11:56:04 +0300

    test

--- _views/domain-view.php
+++ _views/domain-view.php
@@ -8,30 +8,49 @@
       <?php include '_include/tabs.php'; ?>  
         <div class="container">
           <div class="row">
+            <div class="col-md-9">
+              <div class="row">
+              <?php foreach($domaininfo as $key => $info){?> 
+                <div class="col-md-6">            
+              
+              <div class="info-container">
+                <div class="info-title"><?=$info["title"]?></div>
+                <?php foreach($info['text'] as $subtitle){?>
+                <div class="info-row">
+                    <div class="info_sub"><?=$subtitle?></div>
+                    <div class="info-val">12/34/2025</div>
+                    <div class="info-icon">
+                    <span class="icon-fi-sr-grid" alt=""></span>
+                    </div>
+
+                  </div>
+
+                <?php }?>
+                </div>
+              </div> 
+        <?php }?>
+
+
+              </div>
+
+            </div>
+            <div class="col-md-3">
+            <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+                
+             </div>
             
-              <?php foreach($domaininfo as $key => $info){?>              
-                <?php if($key==0) echo '<div class="col-md-5">'?>
-                    <div class="info-container">
-                    <div class="info-title"><?=$info["title"]?></div>
-                    <?php foreach($info['text'] as $subtitle){?>
-                     <div class="info-row">
-                        <div class="info_sub"><?=$subtitle?></div>
-                        <div class="info-val">12/34/2025</div>
-                        <span class="icon-fi-sr-grid" alt=""></span>
-
-                      </div>
-
-                    <?php }?>
-                    </div>               
-                <?php if($key==1) echo '</div><div class="col-md-5">'?>
-                <?php if($key==4) echo '</div>'?>
-               
-
-              <?php }?>
+        
             </div>
-            <div class="col-md-2">
-                test
-            </div>
+       
             
 
            

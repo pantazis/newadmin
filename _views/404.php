70eff2294366b464f4535cdcb64a9a3eda94eb59
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-26 12:36:01 +0300

    update 404errorpage

--- _views/404.php
+++ _views/404.php
@@ -1,2 +1,21 @@
 <div class=errorpage>
+    <style>
+        .logo-button {
+            display: none;
+        }
+
+        .sidebar.navbar {
+            display: none;
+        }
+
+        .sidebar.top.shadow-2-strong {
+            display: none;
+        }
+
+        main {
+            padding-top: 0;
+            padding-left: 0;
+            transition-property: padding-left;
+        }
+    </style>
 </div>

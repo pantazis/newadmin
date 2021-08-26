12e8c3feaac5f392e534e3753b5a0a7bf55748df
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-26 17:26:11 +0300

    Update root.php

--- _include/root.php
+++ _include/root.php
@@ -1,40 +1,33 @@
 <?php
-
-
-
-
-
-
-switch ($request) { 
-        case $url_without_last_part."/" :      
-            include '_views/domain-view.php'; //dasboard.php
-            break;   
-        case $url_without_last_part.'/domain-list' :
-            include '_views/table-list.php';
-            break;
-        case $url_without_last_part.'/domain' :
-            include '_views/domain-view.php';
-            break; 
-        case $url_without_last_part.'/orders' :
-            include '_views/table-list.php';
-            break;  
-        case $url_without_last_part.'/order' :
-            include '_views/order-view.php';
-            break;
-        case $url_without_last_part.'/ssl' :
-            include '_views/ssl-view.php';
-            break; 
-        case $url_without_last_part.'/ssl-list' :
-            include '_views/table-list.php';
-            break;
-        case $url_without_last_part.'/billing-profile' :
-            include '_views/billing-profile.php';
-            break; 
-        case $url_without_last_part.'/billing-profiles' :
-            include '_views/table-list.php';
-            break;
-       default:
-            include '_views/404.php';
-            break;             
+switch ($request) {
+    case $url_without_last_part . "/":
+        include '_views/domain-view.php'; //dasboard.php
+        break;
+    case $url_without_last_part . '/domain-list':
+        include '_views/table-list.php';
+        break;
+    case $url_without_last_part . '/domain':
+        include '_views/domain-view.php';
+        break;
+    case $url_without_last_part . '/orders':
+        include '_views/table-list.php';
+        break;
+    case $url_without_last_part . '/order':
+        include '_views/order-view.php';
+        break;
+    case $url_without_last_part . '/ssl':
+        include '_views/ssl-view.php';
+        break;
+    case $url_without_last_part . '/ssl-list':
+        include '_views/table-list.php';
+        break;
+    case $url_without_last_part . '/billing-profile':
+        include '_views/billing-profile.php';
+        break;
+    case $url_without_last_part . '/billing-profiles':
+        include '_views/table-list.php';
+        break;
+    default:
+        include '_views/404.php';
+        break;
 };
-?>

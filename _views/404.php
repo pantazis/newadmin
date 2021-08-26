cdac14a3f225b5dd39f416a0f5beb2e264d5429c
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-08-26 15:17:48 +0300

    404page

--- _views/404.php
+++ _views/404.php
@@ -18,4 +18,37 @@
             transition-property: padding-left;
         }
     </style>
-</div>
+
+
+    <?php $arr2 = [
+        "Domains",
+        "Hosting",
+        "SSL Certificates",
+        "Users",
+        "Οικονομικά",
+        "Προϊόντα"
+    ];
+    ?>
+
+
+
+
+
+    <div class="textdiv">
+        <h5>Error 404</h5>
+
+        <h1>Page not Found</h1>
+        <p>Δεν μπορούμε να βρούμε την σελίδα που ψάχνεις!</p>
+        <h3>Μπορεί να έχει γίνει κάποιο λάθος στη διεύθυνση ή η σελίδα αυτή να έχει αλλάξει θέση. Μην σε πτοεί όμως αυτό, επίλεξε ένα σύνδεσμο και συνέχισε την πλοήγησή σου:</h3>
+
+        <div class="buttonserror">
+            <?php
+            foreach ($arr2 as $value) { ?>
+
+                <button type=button class="btn actions btn-outline-black_blue"><?= $value ?></button>
+                </button>
+
+            <?php } ?>
+        </div>
+
+    </div>

3143749e051fe4faeb7241e361306c8f2896ed7f
pantazis <pantazisvastardis@yahoo.com>
2021-09-03 17:01:59 +0300

    test

--- _include/form-elements.php
+++ _include/form-elements.php
@@ -1,21 +1,21 @@
 <?php 
 
 function buttonsEl(){ 
-    return   '<button type="button" class="btn btn-outline-blue_purple">add</button><button type="button" class="btn btn-outline-blue_purple">cancel</button>';
+    return   '<div class="pl-1"><button type="button" class="btn btn-outline-blue_purple">add</button></div>';
 }
 
 
 
 function selectEl($arr,$title) {
 
-$template = '<div class="info-text-in"><span class="info_sub">'.$title.'</span>';
+$template = '<div class="info-text-in">';
 $template.='<select class="form-select" aria-label="">';
 foreach($arr as $key => $val){
  $value=$key+1;
  $template.='<option '.($value==1?"selected":"").' value="'.$value.'">'.$val.'</option>'; 
  
 }; 
-$template.='</select></div>'.buttonsEl();
+$template.='</select>'.buttonsEl().'</div>';
 
 
 
@@ -28,7 +28,7 @@ return $template;
 
 
 function inputEl($title,$name,$type,$validateLength,$validate) {
-    $template ='<div class="info-text-in"><span class="info_sub">'.$title.'</span>';
+    $template ='<div class="info-text-in">';
     $template .= '<div class="material-text-field__container">
                 <input id="input-password" data-input-num="5" autocomplete="'.$type.'" type="'.$type.'" placeholder=" " class="material-text-field__input-text" data-last-val="" '.($validateLength!=null ? 'data-validate-length="' .$validateLength.'"':"").'  data-validate="'.$validate.'">
                 <div id="label-text" class="material-text-field__label-text">'.$name.'</div>
@@ -40,7 +40,7 @@ function inputEl($title,$name,$type,$validateLength,$validate) {
                    
                 </div>
             </div>';
-    $template.='</div>'.buttonsEl();
+    $template.=buttonsEl().'</div>';
     return $template;
 }
 

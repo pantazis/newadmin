95b785cd7a827fc5aaadf9c64b14c21a333f3cee
pantazis <pantazisvastardis@yahoo.com>
2021-09-16 16:23:04 +0300

    test

--- _include/tabs.php
+++ _include/tabs.php
@@ -1,44 +1,35 @@
 <style>
-   <?php $tabnum = count($tabs)-1;?>
-   :root {
+<?php $tabnum = count($tabs)-1;?>
+:root{
+  
+   --tab-width-decktop:<?=16.6666*($tabnum+1)?>%;
+   <?php if(60*($tabnum+1)<100){
+      $mobile = 100;
 
-      --tab-width-decktop:
-         <?=16.6666*($tabnum+1)?>
-         %;
-      <?php if (60*($tabnum+1)<100) {
-    $mobile = 100;
-} else {
-    $mobile = 60*($tabnum+1);
-}
+   }else{
+      $mobile = 60*($tabnum+1);
 
-   if (40*($tabnum+1)<100) {
-       $tablet = 100;
-   } else {
-       $tablet = 40*($tabnum+1);
    }
+
+   if(40*($tabnum+1)<100){
+      $tablet = 100;
+
+   }else{
+      $tablet = 40*($tabnum+1);
+
+   }  
    ?>
-      --tab-width:
-         <?= $mobile?>
-         %;
-      --tab-width-tablet:
-         <?=$tablet?>
-         %;
-   }
+   --tab-width:<?= $mobile?>%;      
+   --tab-width-tablet:<?=$tablet?>%;
+}
 </style>
 
 <div class="tab-parent">
-   <div class="tab-cont">
-      <div class="activebox" style="width:<?=(100/($tabnum+1))?>%">
-      </div>
-      <?php foreach ($tabs as $tab => $value) { ?>
-      <div
-         data-tab-id="<?=urlencode($value["name"])?>"
-         class="tab-in <?php if ($tab==0) {
-       echo "active";
-   }?>" data-active="<?=$tab?>">
-         <div class="over"><?=$value["name"]?>
-         </div>
-      </div>
-      <?php } ?>
-   </div>
+<div class="tab-cont">   
+    <div class="activebox" style="width:<?=(100/($tabnum+1))?>%"></div>    
+   <?php foreach($tabs as $tab => $value) { ?>      
+      <div data-tab-id="<?=urlencode($value["name"])?>" class="tab-in <?php if($tab==0) echo "active";?>" data-active="<?=$tab?>"><div class="over"><?=$value["name"]?></div></div>
+   <?php } ?>    
 </div>
+</div>
+

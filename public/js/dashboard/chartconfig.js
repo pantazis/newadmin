5520e2cf35044984b0c15e71db157b27dd717a82
panagiotisgazis <panagiwthsgazhs@gmail.com>
2021-10-26 12:35:33 +0300

    size_labels

--- public/js/dashboard/chartconfig.js
+++ public/js/dashboard/chartconfig.js
@@ -1,90 +1,97 @@
-var chartConfig ={};
-chartConfig.chart1 =  {
-    type: 'bar',
-    data: localData.chart1,
-    options: {
-      maintainAspectRatio:false,
-      plugins: {
-        title: {
-          display: false,
-          text: 'Chart.js Bar Chart - Stacked'
-        },
-      },
-      responsive: true,
-      interaction: {
-        intersect: false,
-      },
-      scales: {
-        x: {
-          stacked: true,
-        },
-        y: {
-          stacked: true
-        }
-      }
-    }
-  };
+var chartConfig = {};
+chartConfig.chart1 = {
+	type: "bar",
+	data: localData.chart1,
+	options: {
+		maintainAspectRatio: false,
+		plugins: {
+			legend: {
+				display: true,
+				labels: {
+					boxWidth: 10,
+				},
+			},
+			title: {
+				display: false,
+				text: "Chart.js Bar Chart - Stacked",
+			},
+		},
+		responsive: true,
+		interaction: {
+			intersect: false,
+		},
+		scales: {
+			x: {
+				stacked: true,
+			},
+			y: {
+				stacked: true,
+			},
+		},
+	},
+};
 
 //----------------------------------chart2-------------------------------------------------//
 
+chartConfig.chart2 = {
+	type: "line",
+	data: localData.chart2,
+	options: {
+		maintainAspectRatio: false,
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "top",
+			},
+			title: {
+				display: false,
+				text: "Chart.js Line Chart",
+			},
+		},
+	},
+};
 
+//----------------------------------------------chart3--------------------------//
+chartConfig.chart3 = {
+	type: "doughnut",
+	data: localData.chart3,
+	options: {
+		maintainAspectRatio: false,
+		responsive: true,
+		plugins: {
+			legend: {
+				display: true,
+				labels: {
+					boxWidth: 10,
+					maxHeight: 50,
+				},
+				position: "right",
+			},
+			title: {
+				display: false,
+				text: "Chart.js Doughnut Chart",
+			},
+		},
+	},
+};
+//----------------------------------chart4-----------------------------------------------
 
-chartConfig.chart2= {
-    type: 'line',
-    data: localData.chart2,
-    options: {
-      maintainAspectRatio:false,
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: false,
-          text: 'Chart.js Line Chart'
-        }
-      }
-    },
-  };
-
-  //----------------------------------------------chart3--------------------------//
-  chartConfig.chart3 = {
-    type: 'doughnut',
-    data: localData.chart3,
-    options: {
-      maintainAspectRatio:false,
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'right',
-        },
-        title: {
-          display: false,
-          text: 'Chart.js Doughnut Chart'
-        }
-      }
-    },
-  };
-  //----------------------------------chart4-----------------------------------------------
-
-  chartConfig.chart4 = {
-    type: 'line',
-    data: localData.chart4 ,
-    options: {
-      maintainAspectRatio:false,
-      plugins: {
-        filler: {
-          propagate: false,
-        },
-        title: {
-          display: false,
-          text: (ctx) => 'Fill: ' + 'start'
-        }
-      },
-      interaction: {
-        intersect: false,
-      }
-    },
-    
-  };
- 
+chartConfig.chart4 = {
+	type: "line",
+	data: localData.chart4,
+	options: {
+		maintainAspectRatio: false,
+		plugins: {
+			filler: {
+				propagate: false,
+			},
+			title: {
+				display: false,
+				text: (ctx) => "Fill: " + "start",
+			},
+		},
+		interaction: {
+			intersect: false,
+		},
+	},
+};

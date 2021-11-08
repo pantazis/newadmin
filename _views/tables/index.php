618ef27756d421542de1f6646650e57d2cba7db5
pantazis <pantazisvastardis@yahoo.com>
2021-11-08 16:55:58 +0200

    test

--- _views/tables/index.php
+++ _views/tables/index.php
@@ -12,14 +12,13 @@
             <?php include '_include/filters.php'; ?>
       <section class="section-main tables">
             <?php include '_views/'.$folder.'/tab1.php'; ?>
-            <div class="selectionActionsContainer">
-                  <div class="actions">
-              <?php    foreach ($actions as $action){ ?>
-                   <div class="actionBtn <?php echo $action ?>"><?php echo $action ?></div>
-           <?php   }?>
-    </div>
-
-
-  </div>
+            
 
       </section>
+      <div class="selectionActionsContainer">
+                  <div class="actions">
+                                           <div class="actionBtn Action 1">Action 1</div>
+                                            <div class="actionBtn Action 2">Action 2</div>
+                                            <div class="actionBtn Action 3">Action 3</div>
+                                     </div>
+            </div>
